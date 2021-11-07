b4b65eabb3d7165d418159c6b9bf4daa7eea593d
Guilherme Rodriguero <gui.rodriguero@hotmail.com>
2021-11-07 15:10:43 -0300

    Project Setup

---
+++ doeaqui/src/main/java/br/com/fiap/doeaqui/DoeaquiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.doeaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoeaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoeaquiApplication.class, args);
+	}
+
+}
